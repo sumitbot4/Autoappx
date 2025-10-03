6b56d7b892d68fbaffdad65f2c12dddbcdfa7b33
sumitbot4 <870.sumit123@gmail.com>
2025-10-04 00:08:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,30 @@
 import os
-##Code Written By @ItsMeMaster
-##Code Written By @ItsMeMaster
+
+## Code Written By @ItsMeMaster (modified to support env vars)
 
 class Config(object):
-    BOT_TOKEN = "8434843795:AAFamCgCGaQXXvTDkJ4FWpHnu_Dv4MmKxxE"
-    DB_NAME = "txtbot"
-    API_ID = "22849789"
-    API_HASH = "0fc127c6055acd59f00ec6c229e1e3c4"
-    ADMIN_ID = 7296271316
-    DB_URL = "mongodb+srv://autoappx750:Yash2835P@autoappx.drvhbnu.mongodb.net/?retryWrites=true&w=majority&appName=Autoappx"
-    LOG_CHANNEL = "-1002477114210" # Your Log Channel ID (Bot ko ADMIN BNAYE)
-    USERLINK = ""
-    TUTORIAL_VIDEO = ""
+    # Bot token from BotFather
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "8434843795:AAFamCgCGaQXXvTDkJ4FWpHnu_Dv4MmKxxE")
 
+    # MongoDB database name
+    DB_NAME = os.getenv("DB_NAME", "txtbot")
 
+    # Telegram API credentials (from https://my.telegram.org)
+    API_ID = int(os.getenv("API_ID", "22849789"))
+    API_HASH = os.getenv("API_HASH", "0fc127c6055acd59f00ec6c229e1e3c4")
 
+    # Admin user ID
+    ADMIN_ID = int(os.getenv("ADMIN_ID", "7296271316"))
 
+    # MongoDB connection string
+    DB_URL = os.getenv(
+        "DB_URL",
+        "mongodb+srv://autoappx750:Yash2835P@autoappx.drvhbnu.mongodb.net/?retryWrites=true&w=majority&appName=Autoappx"
+    )
+
+    # Log channel ID
+    LOG_CHANNEL = os.getenv("LOG_CHANNEL", "-1002477114210")
+
+    # Optional links
+    USERLINK = os.getenv("USERLINK", "")
+    TUTORIAL_VIDEO = os.getenv("TUTORIAL_VIDEO", "")
